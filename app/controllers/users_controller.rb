2bfa9cf38777a5a4d31afca5f74e298cc22fc409
yingjiac <yingjiac@uchicago.edu>
2022-05-10 04:57:05 +0000

    User part half way

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :username => :asc})
+    render({ :template => "user_templates/index.html.erb"})
+  end
+
+  def show
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username})
+
+    @the_user = matching_usernames.at(0)
+
+    #if @the_user == nil
+    #  redirect_to("/")
+    #else
+      render({ :template => "user_templates/show.html.erb"})
+    #end
+
+  end  
+
+
+end  
